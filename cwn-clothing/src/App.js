0a83a086043755a035c313e0d18ead9081432b80
gabriel <gabrielcmoris@gmail.com>
2023-10-23 20:02:27 +0200

    add scss, style the categories, make categories DRY

--- cwn-clothing/src/App.js
+++ cwn-clothing/src/App.js
@@ -1,36 +1,23 @@
+import "./categories.styles.scss";
+import { categories } from "./static-data/categories";
+
 const App = () => {
   return (
     <div className="categories-container">
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      {categories.map(({ title, id, imageUrl }) => {
+        return (
+          <div key={id} className="category-container">
+            <div
+              className="background-image"
+              style={{ backgroundImage: `url(${imageUrl})` }}
+            />
+            <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };

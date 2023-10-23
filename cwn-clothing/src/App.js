69882607698348ad363102fd721289a06e310db5
gabriel <gabrielcmoris@gmail.com>
2023-10-23 20:33:03 +0200

    move to components and modularice to categories and directory

--- cwn-clothing/src/App.js
+++ cwn-clothing/src/App.js
@@ -1,25 +1,8 @@
-import "./categories.styles.scss";
 import { categories } from "./static-data/categories";
+import Directory from "./components/directory/directory.component";
 
 const App = () => {
-  return (
-    <div className="categories-container">
-      {categories.map(({ title, id, imageUrl }) => {
-        return (
-          <div key={id} className="category-container">
-            <div
-              className="background-image"
-              style={{ backgroundImage: `url(${imageUrl})` }}
-            />
-            <div className="category-body-container">
-              <h2>{title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;

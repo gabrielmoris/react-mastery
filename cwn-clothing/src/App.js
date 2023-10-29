b2e427fa9e23a2298d99dc974bd2f16d38db86c5
gabriel <gabrielcmoris@gmail.com>
2023-10-29 20:32:11 +0100

    route and navigation

--- cwn-clothing/src/App.js
+++ cwn-clothing/src/App.js
@@ -1,10 +1,19 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => {
+  return <h1>I am the shop</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        {" "}
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };

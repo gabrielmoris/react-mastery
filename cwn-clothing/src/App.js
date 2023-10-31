9b257fd2e90f6efa01dea64ab47d24424fe85328
gabriel <gabrielcmoris@gmail.com>
2023-10-31 22:41:56 +0100

    start auth with google

--- cwn-clothing/src/App.js
+++ cwn-clothing/src/App.js
@@ -1,7 +1,7 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
-import SignIn from "./routes/sign-in/sign-on.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const Shop = () => {
   return <h1>I am the shop</h1>;

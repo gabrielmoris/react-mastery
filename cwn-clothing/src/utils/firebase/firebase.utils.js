508d14e35606b2dbf7aae890657e8f2bcf9d0748
gabriel <gabrielcmoris@gmail.com>
2023-11-01 22:36:40 +0100

    enable signin with firebase and store the user in DB

--- cwn-clothing/src/utils/firebase/firebase.utils.js
+++ cwn-clothing/src/utils/firebase/firebase.utils.js
@@ -1,8 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-console.log(process.env.REACT_APP_APIKEY);
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
@@ -21,3 +23,22 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (e) {
+      console.log("Error creating the user in DB: ", e);
+    }
+  }
+
+  return userDocRef;
+};

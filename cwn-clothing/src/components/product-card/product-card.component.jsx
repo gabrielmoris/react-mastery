499788fb753027b393dc822eec2805806c21cecc
gabriel <gabrielcmoris@gmail.com>
2023-11-27 17:21:16 +0100

    add itemos to cart logic

--- cwn-clothing/src/components/product-card/product-card.component.jsx
+++ cwn-clothing/src/components/product-card/product-card.component.jsx
@@ -1,8 +1,14 @@
 import "./product-card.styles.scss";
 import Button from "../button/button.component";
+import { useContext } from "react";
+import { CartContext } from "../../context/cart.context";
 
 export const ProductCard = ({ product }) => {
   const { name, imageUrl, price } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={name + "-img"} />
@@ -10,7 +16,9 @@ export const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to Cart</Button>
+      <Button onClick={addProductToCart} buttonType="inverted">
+        Add to Cart
+      </Button>
     </div>
   );
 };

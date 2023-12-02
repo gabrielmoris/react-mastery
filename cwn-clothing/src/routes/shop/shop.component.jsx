dc1dde34ce0eda7433af9b929b6ce6d4ba61c79f
gabriel <gabrielcmoris@gmail.com>
2023-12-02 21:19:48 +0100

    finish categories and routes

--- cwn-clothing/src/routes/shop/shop.component.jsx
+++ cwn-clothing/src/routes/shop/shop.component.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import "./shop.styles.scss";
 import { CategoriesPreview } from "../../routes/categories-preview/categories-preview.component";
+import { Category } from "../../routes/category/catgory.component";
 
 export default function Shop() {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
     </Routes>
   );
 }

cbab114190fa52d9e9bcf34cb5f19ae36a76c03f
gabriel <gabrielcmoris@gmail.com>
2023-12-09 14:16:47 +0100

    redux in cart

---
+++ cwn-clothing/src/store/cart/cart.action.js
@@ -0,0 +1,47 @@
+import { createAction } from "../../utils/reducer/reducer.utils";
+import { CART_ACTION_TYPES } from "./cart.types";
+
+const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find((cartItem) => {
+    return cartItem.id === productToAdd.id;
+  });
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) => (cartItem.id === productToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem));
+  }
+
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
+const removeCartItem = (cartItems, productToRemove) => {
+  const existingCartItem = cartItems.find((cartItem) => {
+    return cartItem.id === productToRemove.id;
+  });
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== productToRemove.id);
+  }
+
+  return cartItems.map((cartItem) => (cartItem.id === productToRemove.id ? { ...cartItem, quantity: cartItem.quantity - 1 } : cartItem));
+};
+
+const clearCartItem = (cartItems, productToRemove) => {
+  return cartItems.filter((cartItem) => cartItem.id !== productToRemove.id);
+};
+
+export const setIsCartOpen = (boolean) => createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, boolean);
+
+export const addItemToCart = (cartItems, productToAdd) => {
+  const newCartItems = addCartItem(cartItems, productToAdd);
+  createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+};
+
+export const removeItemFromCart = (cartItems, productToRemove) => {
+  const newCartItems = removeCartItem(cartItems, productToRemove);
+  createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+};
+
+export const clearItemFromCart = (cartItems, productToRemove) => {
+  const newCartItems = clearCartItem(cartItems, productToRemove);
+  createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+};

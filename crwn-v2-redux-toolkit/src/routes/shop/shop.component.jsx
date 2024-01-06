2654f03009faf675ac1464d043810464b7244253
gabriel <gabrielcmoris@gmail.com>
2024-01-06 22:20:04 +0100

    solve warnings from console (unused variables and react-hooks/exhaustive-deps). Modify user with redux toolkit

--- crwn-v2-redux-toolkit/src/routes/shop/shop.component.jsx
+++ crwn-v2-redux-toolkit/src/routes/shop/shop.component.jsx
@@ -1,28 +1,29 @@
-import { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-import CategoriesPreview from '../categories-preview/categories-preview.component';
-import Category from '../category/category.component';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories } from '../../store/categories/category.action';
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { setCategories } from "../../store/categories/category.action";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments('categories');
+      const categoriesArray = await getCategoriesAndDocuments("categories");
       dispatch(setCategories(categoriesArray));
     };
 
     getCategoriesMap();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
-      <Route path=':category' element={<Category />} />
+      <Route path=":category" element={<Category />} />
     </Routes>
   );
 };
--- crwn-v2-redux-toolkit/src/store/store.js
+++ crwn-v2-redux-toolkit/src/store/store.js
@@ -1,24 +1,38 @@
-import { compose, createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import logger from "redux-logger";
+// import { compose, createStore, applyMiddleware } from "redux";
+// import { persistStore, persistReducer } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+// import logger from "redux-logger";
 
+// import { rootReducer } from "./root-reducer";
+
+// const middleWares = [process.env.NODE_ENV === "development" && logger].filter(Boolean);
+
+// const composeEnhancer = (process.env.NODE_ENV !== "production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   blacklist: ["user"],
+// };
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+// export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+// export const persistor = persistStore(store);
+
+import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./root-reducer";
+import { loggerMiddleware } from "./middleware/logger";
 
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(Boolean);
+const middleWares = [process.env.NODE_ENV === "development" && loggerMiddleware].filter(Boolean);
 
-const composeEnhancer = (process.env.NODE_ENV !== "production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-
-const persistConfig = {
-  key: "root",
-  storage,
-  blacklist: ["user"],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
-
-export const store = createStore(persistedReducer, undefined, composedEnhancers);
-
-export const persistor = persistStore(store);
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // whatever I ecome from the firebase User is not a clean object, I can Pick the parts of the user or just disable this middleware
+    }).concat(middleWares),
+});

e4b14e206b74d09f19361c39231a1ca4303734b4
gabriel <gabrielcmoris@gmail.com>
2024-01-09 22:02:44 +0100

    add function for stripe

--- crwn-v2-redux-toolkit/src/components/payment-form/payment-form.component.jsx
+++ crwn-v2-redux-toolkit/src/components/payment-form/payment-form.component.jsx
@@ -10,16 +10,47 @@ export const PaymentForm = () => {
   const paymentHandler = async (e) => {
     e.preventDefault();
     if (!stripe || !elements) return;
+
+    // I fetch where I have the netlify cloud function
+    const res = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 100 }),
+    }).then((res) => res.json());
+
+    //I get tye client_secret from the response
+    const {
+      paymentIntent: { client_secret },
+    } = res;
+
+    // I confirm the card payment, for that I pass the client_secret and the actual component CardElement
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "GabrielCMoris",
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        // Card: 4242424242424242 Date: 04/24 CVC: 242  C.P: 42424
+        alert("Payment Successful");
+      }
+    }
   };
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment:</h2>
         <CardElement />
-        <Button onClick={paymentHandler} buttonType={BUTTON_TYPE_CLASSES.inverted}>
-          Pay Now
-        </Button>
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
       </FormContainer>
     </PaymentFormContainer>
   );

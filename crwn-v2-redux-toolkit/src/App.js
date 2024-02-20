87fc7d341a11e587f89ff6133c6f88a622bcd744
gabriel <gabrielcmoris@gmail.com>
2024-02-20 12:54:33 +0100

    lazy loading and dynamic imports

--- crwn-v2-redux-toolkit/src/App.js
+++ crwn-v2-redux-toolkit/src/App.js
@@ -1,15 +1,20 @@
-import { useEffect } from "react";
+import { Suspense, lazy, useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 import { Routes, Route } from "react-router-dom";
 
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "./utils/firebase/firebase.utils";
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/checkout/checkout.component";
 import { setCurrentUser } from "./store/user/user.reducer";
+import { Spinner } from "./components/spinner/spinner.component";
+
+// Normal imports blocks the app until the import is finished. Javascript now allows Dynamic Imports:
+// const Home = await import("./routes/home/home.component"); React doesnt understand this feature, so I need to use lazy to make it work
+// it is important to add <Suspense> from React in top level to make it work
+const Home = lazy(() => import("./routes/home/home.component"));
+const Navigation = lazy(() => import("./routes/navigation/navigation.component"));
+const Authentication = lazy(() => import("./routes/authentication/authentication.component"));
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -27,14 +32,16 @@ const App = () => {
   }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 

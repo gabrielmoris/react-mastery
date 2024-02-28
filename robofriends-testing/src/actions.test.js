d3c96a1d6100db06fc1c886f7a8c6f058c1b9e39
gabriel <gabrielcmoris@gmail.com>
2024-02-28 23:48:22 +0100

    test actions

--- robofriends-testing/src/actions.test.js
+++ robofriends-testing/src/actions.test.js
@@ -1,26 +1,37 @@
-import * as actions from './actions'
-import * as types from './constants'
-import configureMockStore from 'redux-mock-store'
-import thunkMiddleware from 'redux-thunk'
+import * as actions from "./actions";
+import { CHANGE_SEARCHFIELD, REQUEST_ROBOTS_PENDING } from "./constants";
+import configureMockStore from "redux-mock-store";
+import thunkMiddleware from "redux-thunk";
 
 export const mockStore = configureMockStore([thunkMiddleware]);
 
-describe('actions', () => {
-  it('should create an action to search', () => {
-    const text = 'Finish docs'
+describe("actions", () => {
+  it("should create an action to search", () => {
+    const text = "Finish docs";
     const expectedAction = {
-      type: types.CHANGE_SEARCHFIELD,
-      payload: text
-    }
-    expect(actions.setSearchField(text)).toEqual(expectedAction)
+      type: CHANGE_SEARCHFIELD,
+      payload: text,
+    };
+    expect(actions.setSearchField(text)).toEqual(expectedAction);
+  });
+});
+
+if (
+  ("handles requesting robots API",
+  () => {
+    const expectedAction = {
+      type: REQUEST_ROBOTS_PENDING,
+    };
+    expect(actions.requestRobots()).toEqual(expectedAction);
   })
-})
+);
 
 describe("Fetch robots action PENDING", () => {
   it("should creat a Pending action on request Robots", () => {
     const store = mockStore();
-    store.dispatch(actions.requestRobots())
+    store.dispatch(actions.requestRobots());
     const action = store.getActions();
-    expect(action[0]).toEqual({type: "REQUEST_ROBOTS_PENDING"});
+    const expectedAction = { type: "REQUEST_ROBOTS_PENDING" };
+    expect(action[0]).toEqual(expectedAction);
   });
 });

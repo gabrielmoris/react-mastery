19c3f994ca3a48bd5a8523ba3a0caf4500fedd32
gabriel <gabrielcmoris@gmail.com>
2024-03-02 10:45:35 +0100

    test button

--- testing-crwn-clothing/src/components/button/__tests__/button.test.tsx
+++ testing-crwn-clothing/src/components/button/__tests__/button.test.tsx
@@ -1,29 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import Button, { BUTTON_TYPE_CLASSES } from '../button.component';
+import { render, screen } from "@testing-library/react";
+import Button, { BUTTON_TYPE_CLASSES } from "../button.component";
 
-describe('button tests', () => {
-  test('should render base button when nothing is passed', () => {
+describe("button tests", () => {
+  test("should render base button when nothing is passed", () => {
     render(<Button>Test</Button>);
 
-    expect(screen.getByRole('button')).toHaveStyle('background-color: black');
-    expect(screen.getByRole('button')).not.toBeDisabled();
+    const btn = screen.getByRole("button");
+
+    expect(btn).toHaveStyle("background-color: black");
+    expect(btn).not.toBeDisabled();
   });
 
-  test('should be disabled if isLoading is true', () => {
-    render(<Button isLoading={true}>Test</Button>);
-
-    expect(screen.getByRole('button')).toBeDisabled();
-  });
-
-  test('should render google button when passed google type', () => {
+  test("should render google button when passed google type", () => {
     render(<Button buttonType={BUTTON_TYPE_CLASSES.google}>Test</Button>);
-
-    expect(screen.getByRole('button')).toHaveStyle('background-color: #4285f4');
+    const googleButton = screen.getByRole("button");
+    expect(googleButton).toHaveStyle("background-color: #4285f4");
   });
 
-  test('should render inverted button when passed inverted type', () => {
+  test("should render inverted button when passed inverted type", () => {
     render(<Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Test</Button>);
 
-    expect(screen.getByRole('button')).toHaveStyle('background-color: white');
+    const invertedBtn = screen.getByRole("button");
+    expect(invertedBtn).toHaveStyle("background-color: white");
+  });
+
+  test("should be disabled if isLoading is true", () => {
+    render(<Button isLoading={true}>Test</Button>);
+    const loadingBtn = screen.getByRole("button");
+    expect(loadingBtn).toBeDisabled();
   });
 });

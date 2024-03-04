1586ae65822ea13cd0e97e3e4060ed8bb1b6ad0e
gabriel <gabrielcmoris@gmail.com>
2024-03-04 20:20:11 +0100

    navigation component test finish

--- testing-crwn-clothing/src/routes/navigation/__tests__/navigation.test.component.js
+++ testing-crwn-clothing/src/routes/navigation/__tests__/navigation.test.component.js
@@ -3,7 +3,7 @@ import Navigation from "../navigation.component";
 import { renderWithProviders } from "../../../utils/test/test.utils";
 
 describe("navigation tests", () => {
-  test("It should render a Sign in link if there is no user", () => {
+  test("It should render a Sign in link and not a sign Out link if there is no user", () => {
     renderWithProviders(<Navigation />, {
       preloadedState: {
         user: {
@@ -11,7 +11,9 @@ describe("navigation tests", () => {
         },
       },
     });
-
+    // The difference between queryByTest and getByText is that queryByTest doesnt show an error if it doesnt find the text
+    const signOutLinkElement = screen.queryByText(/sign out/i);
+    expect(signOutLinkElement).toBeNull();
     const signInLinkElement = screen.getByText(/sign in/i);
     expect(signInLinkElement).toBeInTheDocument();
   });
@@ -25,7 +27,29 @@ describe("navigation tests", () => {
       },
     });
 
+    // The difference between queryByTest and getByText is that queryByTest doesnt show an error if it doesnt find the text
+    const signInLinkElement = screen.queryByText(/sign in/i);
+    expect(signInLinkElement).toBeNull();
     const signOutLinkElement = screen.getByText(/sign out/i);
     expect(signOutLinkElement).toBeInTheDocument();
+  });
+
+  test("it should not render a cart dropdown if isOpen is false", () => {
+    renderWithProviders(<Navigation />, {
+      preloadedState: {
+        cart: { isCartOpen: false, cartItems: [] },
+      },
+    });
+    const dropdownText = screen.queryByText(/your cart is empty/i);
+    expect(dropdownText).toBeNull();
+  });
+  test("it should render a cart dropdown if isOpen is true", () => {
+    renderWithProviders(<Navigation />, {
+      preloadedState: {
+        cart: { isCartOpen: true, cartItems: [] },
+      },
+    });
+    const dropdownText = screen.getByText(/your cart is empty/i);
+    expect(dropdownText).toBeInTheDocument();
   });
 });
